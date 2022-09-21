ac5e0fa8a1d096247f37827512565c43fa3e4680
Majid Rajendra Rahmat <majid.rajendra@ui.ac.id>
2022-09-22 00:19:16 +0700

    Added tests

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from mywatchlist.views import show_mywatchlist
 
 # Create your tests here.
+class AppTest(TestCase):
+    def test_html_200(self):
+        response = Client().get('https://tugas-2-pbp-majid.herokuapp.com/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_xml_200(self):
+        response = Client().get('https://tugas-2-pbp-majid.herokuapp.com/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
+    def test_json_200(self):
+        response = Client().get('https://tugas-2-pbp-majid.herokuapp.com/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
